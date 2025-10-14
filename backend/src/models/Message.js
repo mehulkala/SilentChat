9138546c01e97653f5dcaf9eb2d8c2f4017e8d4f
mehulkala <mehulkala22@gmail.com>
2025-10-14 21:57:43 +0530

    message routes optimized

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -4,7 +4,7 @@ const messageSchema = new mongoose.Schema(
     {
         senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-        text: { type: String },
+        text: { type: String, trim: true, maxlength: 2000 },
         image: { type: String },
     },
     { 
