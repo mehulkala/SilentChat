fc8cf1ec7d16ab6ea4248c3c9b9c531da72284f5
mehulkala <mehulkala22@gmail.com>
2025-10-19 12:11:56 +0530

    chatpage-setup

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,31 @@
 import React, { use } from 'react'
-import { useAuthStore } from '../store/useAuthStore';
+import { useChatStore } from '../store/useChatStore.js';
+import ProfileHeader from '../components/ProfileHeader.jsx';
+import ActiveTabSwitch from '../components/ActiveTabSwitch.jsx';
+import ChatsList from '../components/ChatsList.jsx';
+import ContactList from '../components/ContactList.jsx';
+import ChatContainer from '../components/ChatContainer.jsx';
+import NoConversation from '../components/NoConversation.jsx';
 
 function ChatPage() {
-  const {logout} = useAuthStore();
+  const {activeTab, selectedUser} = useChatStore();
+
   return (
-    <div>ChatPage
-      <button onClick={logout}>logout</button>
+    <div className='relative w-full max-w-6xl h-[800px]'>
+      {/* LEFT SIDE */}
+      <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+        <ProfileHeader />
+        <ActiveTabSwitch />
+
+        <div className="flex-1 overflow-y-auto p-4 space-y-2">
+          { activeTab === "chats" ? <ChatsList /> : <ContactList /> }
+        </div>
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+        { selectedUser ? <ChatContainer /> : <NoConversation /> }
+      </div>
     </div>
   )
 }
